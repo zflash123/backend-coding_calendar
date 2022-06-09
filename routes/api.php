af6a4fdd6faad60e08807f6aa8ef0000d28ace43
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-06-09 13:44:52 +0700

    import UsersController class

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
