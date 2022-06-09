74d5ff22fc60b5205b5510c68324222ad78a3741
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-06-09 12:38:03 +0700

    install laravel passport

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-
 class ApiController extends Controller
 {
     public function user_duration() {
