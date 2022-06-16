1440b34d204551522a0e02510ffa61464ed2537f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-06-16 07:07:16 +0700

    make table schema

--- database/migrations/2022_06_15_232509_create_event_details.php
+++ database/migrations/2022_06_15_232509_create_event_details.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('event_details', function (Blueprint $table) {
             $table->id();
+            $table->string('project_name');
+            $table->time('start_time_plan');
+            $table->time('finish_time_plan');
+            $table->time('start_wakatime');
+            $table->time('finish_wakatime');
             $table->timestamps();
         });
     }
